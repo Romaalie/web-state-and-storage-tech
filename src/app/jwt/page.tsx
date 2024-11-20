995797dfbbbc0a46e40e2cfbf1c49e55723c2585
Ali Romar <ali.romar@myy.haaga-helia.fi>
2024-11-20 09:55:22 +0200

    Add routing structure

---
+++ src/app/jwt/page.tsx
@@ -0,0 +1,3 @@
+export default function Page() {
+    return <h1>This is the page for JWT</h1>
+}
---
+++ src/app/localstorage/page.tsx
@@ -0,0 +1,3 @@
+export default function Page() {
+    return <h1>This is the page for Local Storage</h1>
+}
