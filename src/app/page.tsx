462b87dedb5aedbf4d0fc2a32b19776fac3c3980
Ali Romar <ali.romar@myy.haaga-helia.fi>
2024-11-21 11:46:01 +0200

    Fix main page text centering

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,7 +9,7 @@ export default function Home() {
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-20 row-start-2 items-center sm:items-start">
-        <h1 className="text-3xl sm:text-5xl font-bold text-center sm:text-left">
+        <h1 className="text-3xl sm:text-5xl font-bold text-center sm:text-center max-w-5xl">
           The amazing world of State and Storage Mechanisms in Web Applications</h1>
         <div className="flex items-center justify-center w-full">
           <ul className="grid grid-cols-2 gap-6 items-center justify-center">
